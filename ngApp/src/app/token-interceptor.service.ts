93c65b7238356c0d1fe07cf8fe0ad1873709b565
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-12 14:42:41 +0300

    added real token

--- ngApp/src/app/token-interceptor.service.ts
+++ ngApp/src/app/token-interceptor.service.ts
@@ -1,16 +1,18 @@
 import { HttpInterceptor } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(private authSerice :AuthService) {}
 
   intercept(req, next) {
+    const token = this.authSerice.getToken();
     let tokenizedRequest = req.clone({
       setHeaders: {
-        Authorization: 'Bearer xx.yy.zz',
+        Authorization: `Bearer ${token}`,
       },
     });
     return next.handle(tokenizedRequest);
